aa06177928e08d8b8d543b2fcc4829f7b032a2a8
Bala Krishna <prince.balakrishna@gmail.com>
2023-10-18 13:01:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_account_password_policy" "strict" {
   require_uppercase_characters   = true
   require_symbols                = true
   #allow_users_to_change_password = true
-  #password_reuse_prevention      = 24 
+  password_reuse_prevention      = 24 
   max_password_age = 3
 }
 
